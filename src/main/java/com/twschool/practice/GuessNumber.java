609462e4d32edbd493700ab90447a21039461a8d
sxj415324@163.com <sxj415324@163.com>
2020-06-19 17:24:22 +0800

    should_return_4A0B_given_GuessNumber_1234_when_input_1234

--- src/main/java/com/twschool/practice/GuessNumber.java
+++ src/main/java/com/twschool/practice/GuessNumber.java
@@ -1,11 +1,31 @@
 package com.twschool.practice;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 public class GuessNumber {
     private String guessNumber;
-    public GuessNumber(String guessNumber){
+
+    public GuessNumber(String guessNumber) {
         this.guessNumber = guessNumber;
     }
-    public String compare(String inputNumber){
+
+    public String compare(String inputNumber) {
         return "0A0B";
+    }
+
+    public String check(String inputNumberString) {
+        List<String> inputNumber = Arrays.asList(inputNumberString.split(""));
+        int ValueAndPositionCorrectCount = 0;
+        int ValueIncorrectAndPositionCorrectCount = 0;
+        for (int index = 0; index < inputNumber.size(); index++) {
+            if (inputNumber.get(index).equals(inputNumber.get(index))) {
+                ValueAndPositionCorrectCount ++;
+            } else if (inputNumber.contains(inputNumber.get(index))) {
+                ValueIncorrectAndPositionCorrectCount ++;
+            }
+        }
+        return ValueAndPositionCorrectCount + "A" + ValueIncorrectAndPositionCorrectCount + "B";
     }
 }
--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -5,15 +5,25 @@ import org.junit.Test;
 
 public class GuessNumberTest {
     @Test
-    public void should_return_0A0B_given_GuessNumber_1234_when_input_5678(){
+    public void should_return_0A0B_given_GuessNumber_1234_when_input_5678() {
         //given
         GuessNumber guessNumber = new GuessNumber("1234");
         //when
         String result = guessNumber.compare("5678");
         //then
-        Assert.assertEquals("0A0B",result);
+        Assert.assertEquals("0A0B", result);
         Assert.assertNotNull(result);
     }
 
+    @Test
+    public void should_return_4A0B_given_GuessNumber_1234_when_input_1234() {
+        //given
+        GuessNumber guessNumber = new GuessNumber("1234");
+        //when
+        String result = guessNumber.check("1234");
+        //then
+        Assert.assertEquals("4A0B", result);
+        Assert.assertNotNull(result);
 
+    }
 }
